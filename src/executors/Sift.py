5e7a4548ccef4419cee81475e9171935431605ae
saliha <112631317+salihameryemalbayrak@users.noreply.github.com>
2026-02-09 12:41:02 +0300

    chore: executor

--- src/executors/Sift.py
+++ src/executors/Sift.py
@@ -28,8 +28,6 @@ class Sift(Component):
         self.cfg_sigma = self.request.get_param("configSigma")
         self.cfg_octave = self.request.get_param("configOctaveLayers")
 
-
-
         self.detections = []
         self.outputData = {}
 
@@ -56,44 +54,41 @@ class Sift(Component):
         )
 
     @staticmethod
-    def _is_enabled(cfg) -> bool:
-        # dropdown -> cfg.value is EnableOn or EnableOff
-        return cfg.value.value is True
+    def _is_disabled(cfg) -> bool:
+        return cfg.value.value is False
 
-
-    def sift_inference(self, img):
+    def sift_inference(self):
         img = Image.get_frame(img=self.image, redis_db=self.redis_db)
         frame = self._ensure_uint8(np.asarray(img.value))
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         nfeatures = (
-            self.cfg_max_features.value.maxFeatures.value
-            if self._is_enabled(self.cfg_max_features)
+            self.cfg_max_features.value.configMaxFeaturesValue.value
+            if not self._is_disabled(self.cfg_max_features)
             else 0
         )
 
         contrast = (
-            self.cfg_contrast.value.contrastThreshold.value
-            if self._is_enabled(self.cfg_contrast)
+            self.cfg_contrast.value.configContrastThresholdValue.value
+            if not self._is_disabled(self.cfg_contrast)
             else 0.04
         )
 
         edge = (
-            self.cfg_edge.value.edgeThreshold.value
-            if self._is_enabled(self.cfg_edge)
+            self.cfg_edge.value.configEdgeThresholdValue.value
+            if not self._is_disabled(self.cfg_edge)
             else 10
         )
 
         sigma = (
-            self.cfg_sigma.value.sigma.value
-            if self._is_enabled(self.cfg_sigma)
+            self.cfg_sigma.value.configSigmaValue.value
+            if not self._is_disabled(self.cfg_sigma)
             else 1.6
         )
 
         octave_layers = (
-            self.cfg_octave.value.nOctaveLayers.value
-            if self._is_enabled(self.cfg_octave)
+            self.cfg_octave.value.configOctaveLayersValue.value
+            if not self._is_disabled(self.cfg_octave)
             else 3
         )
 
@@ -116,7 +111,6 @@ class Sift(Component):
 
         vis = cv2.drawKeypoints(frame, kp, None)
 
-        # 7) keypoints -> detections (single detection)
         keypoints_model = [self._kp_to_model(p) for p in kp]
 
         self.detections = [
@@ -129,7 +123,6 @@ class Sift(Component):
             )
         ]
 
-        # 8) descriptors -> outputData
         self.outputData = {
             "descriptors": {
                 "shape": [int(des.shape[0]), int(des.shape[1])],
@@ -138,7 +131,6 @@ class Sift(Component):
             }
         }
 
-        # 9) save output image
         out_img = ImageModel(
             name=img.name,
             uID=img.uID,
@@ -147,12 +139,17 @@ class Sift(Component):
             value=vis,
             type=img.type,
         )
+
         self.image = Image.set_frame(
-            img=out_img, package_uID=self.uID, redis_db=self.redis_db
+            img=out_img,
+            package_uID=self.uID,
+            redis_db=self.redis_db,
         )
 
         return build_response(context=self)
 
+    def run(self):
+        return self.sift_inference()
 
 if "__main__" == __name__:
     Executor(sys.argv[1]).run()
